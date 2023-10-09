a57a3420c17e6b26822abdc47d9ff483a0598d96
ta-design2021 <takenori.aino@gmail.com>
2023-10-09 23:32:24 +0900

    ログイン後にマイページに遷移する設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  def after_sign_in_path_for(resource)
+    user_path(resource) # ログイン後にマイページに遷移
+  end
+
 
   private
 
